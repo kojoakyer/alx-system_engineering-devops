06066bc846c5bd6ee4d91419bba1843fb20baca3
Kojo Akyer <kojoakyer777@gmail.com>
2024-04-24 10:40:54 -0700

    updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-#The code will create a file name school inside /tmp Directory
+# A puppet manifest that creates a file school in /tmp.
+# The file (School) has permissions 0744, belonging to the user and group
+# www-data.
 file { '/tmp/school':
-	ensure => file,
-	content => 'I love Puppet',
-	mode => '07744',
-	owner => 'www-data',
-	group => 'www-data',
-}
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}}
